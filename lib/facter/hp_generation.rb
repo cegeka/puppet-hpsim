5025531aa99fc9850c35ca8e5a9be773d081a930
Ausloos Kim <kim.ausloos@cegeka.be>
2019-01-18 13:45:00 +0100

    [HPSIM] Added generation facts, don't install smh on gen10

---
+++ lib/facter/hp_generation.rb
@@ -0,0 +1,23 @@
+Facter.add(:is_hp_gen10) do
+  setcode do
+    Facter::Core::Execution.execute('cat /sys/devices/virtual/dmi/id/product_name').lines.first.end_with? "Gen10"
+  end
+end
+
+Facter.add(:is_hp_gen9) do
+  setcode do
+    Facter::Core::Execution.execute('cat /sys/devices/virtual/dmi/id/product_name').lines.first.end_with? " Gen9"
+  end
+end
+
+Facter.add(:is_hp_gen8) do
+  setcode do
+    Facter::Core::Execution.execute('cat /sys/devices/virtual/dmi/id/product_name').lines.first.end_with? " Gen8"
+  end
+end
+
+Facter.add(:is_hp_gen7) do
+  setcode do
+    Facter::Core::Execution.execute('cat /sys/devices/virtual/dmi/id/product_name').lines.first.end_with? " G7"
+  end
+end
--- manifests/init.pp
+++ manifests/init.pp
@@ -14,25 +14,28 @@ class hpsim {
 
   case $::operatingsystemmajrelease {
     /5/: {
-      package { [ 'hp-health', 'hp-OpenIPMI', 'hpsmh', 'hp-smh-templates', 'hp-snmp-agents', 'hpvca' ]:
+      package { [ 'hp-health', 'hp-OpenIPMI', 'hp-snmp-agents', 'hpvca' ]:
         ensure => installed,
       }
     }
 
     /6/: {
-      package { [ 'hp-health' , 'hpsmh', 'hp-smh-templates', 'hp-snmp-agents', 'hpvca' ]:
+      package { [ 'hp-health', 'hp-snmp-agents', 'hpvca' ]:
         ensure => installed,
       }
     }
 
     /7/: {
-      package { [ 'hp-health' , 'hpsmh', 'hp-smh-templates', 'hp-snmp-agents', 'kmod-hpvsa' ]:
+      package { [ 'hp-health', 'hp-snmp-agents', 'kmod-hpvsa' ]:
         ensure => installed,
       }
     }
-
     default: { notice("operatingsystemrelease ${::operatingsystemrelease} is not supported") }
-
   }
 
+  if !$::is_hp_gen10 {
+    package { ['hpsmh', 'hp-smh-templates']:
+      ensure => installed,
+    }
+  }
 }
